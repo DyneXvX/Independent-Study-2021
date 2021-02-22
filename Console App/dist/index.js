a335bb9338e327cf90cdb2afd27bc42fa673e656
Justn Thoms <DyneXvX@outlook.com>
2021-02-22 13:21:01 -0500

    final update

--- Console App/dist/index.js
+++ Console App/dist/index.js
@@ -7,7 +7,7 @@ const fs_1 = __importDefault(require("fs"));
 console.log('This is a very simple setup in Node that can be run via node.');
 console.log('Hello World!');
 console.log('However with the power of Node we are able to effect the file system.\n');
-//creates a file in the the file folder called helloWorld.txt and write one line to that file.
+//creates a file in the the folder called helloWorld.txt and writes one line to that file.
 fs_1.default.writeFile('helloWorld.txt', 'Hello World!', function (err) {
     if (err)
         return console.log(err);
--- Console App/src/index.ts
+++ Console App/src/index.ts
@@ -6,7 +6,7 @@ console.log('Hello World!');
 
 console.log('However with the power of Node we are able to effect the file system.\n')
 
-//creates a file in the the file folder called helloWorld.txt and write one line to that file.
+//creates a file in the the folder called helloWorld.txt and writes one line to that file.
 fs.writeFile('helloWorld.txt', 'Hello World!', function(err){
     if (err) return console.log(err);
     console.log('File was created.');
