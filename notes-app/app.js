37c90126ec3919a8a5f3b0222890d9bc99395f45
Justn Thoms <DyneXvX@outlook.com>
2021-02-24 12:54:37 -0500

    buiding commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,41 @@
 const validator = require('validator')
-
+const yargs = require('yargs')
 const getNotes = require('./note.js')
 
+//Create add command
 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note.',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
 
-const command = process.argv[2]
-console.log(process.argv)
-if (command === 'add')
-{
-    console.log('Adding note!')
-} else if (command === 'another')
-{
-    console.log('another command')
-}
+//remove note command
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove Notes',
+    handler: function(){
+        console.log('remove the note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'list all the notes.',
+    handler: function(){
+        console.log('listing all of my notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Reading my notes',
+    handler: function(){
+        console.log('Reading the note')
+    }
+})
+
+console.log(yargs.argv)
