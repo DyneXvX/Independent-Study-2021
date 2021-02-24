564324b1e13d1a85ba716d18adafabb68e6dea6e
Justn Thoms <DyneXvX@outlook.com>
2021-02-24 14:41:51 -0500

    add note is done

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,55 +1,53 @@
-const validator = require('validator')
-const yargs = require('yargs')
-const getNotes = require('./note.js')
+const yargs = require("yargs");
+const notes = require("./note.js");
 
 //Create add command
 
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note.',
-    builder: {
-        title: {
-            describe: 'Note Title',
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: 'This is the notes body',
-            demandOption: true,
-            type: 'string'
-        }
+  command: "add",
+  describe: "Add a new note.",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
     },
-    handler: function(argv){
-        console.log('Title: ' + argv.title)
-        console.log('Body is: ', argv.body)
-    }
-})
+    body: {
+      describe: "This is the notes body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body)
+  },
+});
 
 //remove note command
 
 yargs.command({
-    command: 'remove',
-    describe: 'Remove Notes',
-    handler: function(){
-        console.log('remove the note')
-    }
-})
+  command: "remove",
+  describe: "Remove Notes",
+  handler: function () {
+    console.log("remove the note");
+  },
+});
 
 yargs.command({
-    command: 'list',
-    describe: 'list all the notes.',
-    handler: function(){
-        console.log('listing all of my notes')
-    }
-})
+  command: "list",
+  describe: "list all the notes.",
+  handler: function () {
+    console.log("listing all of my notes");
+  },
+});
 
 yargs.command({
-    command: 'read',
-    describe: 'Reading my notes',
-    handler: function(){
-        console.log('Reading the note')
-    }
-})
+  command: "read",
+  describe: "Reading my notes",
+  handler: function () {
+    console.log("Reading the note");
+  },
+});
 
-yargs.parse()
+yargs.parse();
 //console.log(yargs.argv)
