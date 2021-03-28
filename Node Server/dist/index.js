f4c88e987fae9ffc4552a09dcac9bd85244435e9
Justn Thoms <DyneXvX@outlook.com>
2021-03-28 15:49:50 -0400

    finished setup

---
+++ Node Server/dist/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path')
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(process.cwd() + '/public')));
+app.use("/", (req, res, next) => {    
+  res.sendFile(path.join(process.cwd() + '/public/views/index.html'));
+});
+app.listen(port);
+//# sourceMappingURL=index.js.map
